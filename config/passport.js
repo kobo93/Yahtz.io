ff9dd2cb385502a3b525f853cc65ff60cf095bba
colinboyle <colin.boyle93@gmail.com>
2019-06-16 20:56:05 -0500

    commit this

--- config/passport.js
+++ config/passport.js
@@ -2,7 +2,7 @@ const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
 const mongoose = require("mongoose");
 const User = mongoose.model("users");
-const keys = process.env.sectetKey || require("../config/keys").secretOrKey;
+const keys = process.env.secretKey || require("../config/keys").secretOrKey;
 
 const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
